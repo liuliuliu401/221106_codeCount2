08a7234914be571bd03ddf65bb966a5d380f4e2d
Zihan Liu <3190104622@zju.edu.cn>
2022-11-28 10:39:20 +0800

    CodeCounter v1.1

--- demo/readme.md
+++ demo/readme.md
@@ -1,5 +1,6 @@
 启动代码
-'c:\Users\SubFunction Residue\Documents\AAA My Scripts\Java Scripts\221106_codeCount2'; & 'D:\Program Files\Java\jdk-18.0.2.1\bin\java.exe' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\SubFunction Residue\Documents\AAA My Scripts\Java Scripts\221106_codeCount2\demo\target\classes' 'com.example.CodeCounter' 'C:\Users\SubFunction Residue\Documents\AAA My Scripts\Java Scripts\221106_codeCount2\demo\src\main\java\com\example\CodeCounter.java'
+'c:\Users\SubFunction Residue\Documents\AAA My Scripts\Java Scripts\221106_codeCount2'; & 'D:\Program Files\Java\jdk-18.0.2.1\bin\java.exe' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\SubFunction Residue\Documents\AAA My Scripts\Java Scripts\221106_codeCount2\demo\target\classes' 'com.example.CodeCounter' 'demo\src\main\java\com\example\CodeCounter.java'
+cloc 'demo\src\main\java\com\example\CodeCounter.java'
 
 git config --global user.name "Zihan Liu" 
 git config --global user.email "3190104622@zju.edu.cn"
--- demo/src/main/java/com/example/test.java
+++ demo/src/main/java/com/example/test.java
@@ -1,15 +1,23 @@
 package com.example;
-import java.util.regex.*;
-
-public class test {
-    public static void main(String[] args){
-        String content = "axbxcxdxexfx";
-   
-        String pattern = "((x.*?){2})x";
-   
-        boolean isMatch = Pattern.matches(pattern, content);
-        System.out.println(isMatch);
-     }
-    // ((x.*?){k-1})x
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+ 
+public class test
+{
+    private static final String REGEX = "\\*/";
+    // private static final String INPUT =
+    //                                 "***************";
+ 
+    public static void main( String[] args ){
+       Pattern p = Pattern.compile(REGEX);
+       Matcher m = p.matcher("*/System.out.println(\"/**/\");/*"); // 获取 matcher 对象
+       int count = 0;
+ 
+       while(m.find()) {
+         count++;
+         System.out.println("Match number "+count);
+         System.out.println("start(): "+m.start());
+         System.out.println("end(): "+m.end());
+      }
+   }
 }
-
